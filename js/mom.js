8afa92b73f1bd4c0a8c19a87c6cc8fe6e0630f8a
leslie tu <chiayin.l.tu@gmail.com>
2016-06-26 14:01:53 -0400

    refine header styles; add waypoints and drop in temp placeholder text

--- js/mom.js
+++ js/mom.js
@@ -2,8 +2,11 @@
   'use strict';
 
   $(function () {
-    
-    var $marqueeAlbum = $('#marquee-album');
+
+    var $marqueeAlbum = $('#marquee-album'),
+        $body = $('body'),
+        headerWaypoint;
+
     $marqueeAlbum.slick({
       adaptiveHeight: true,
       appendArrows: $('.marquee-controls'),
@@ -12,5 +15,20 @@
       nextArrow: '<button type="button" class="slick-next p-abs">&#x276f;</button>',
       slide: '.fit-frame',
     });
+
+    // Waypoints
+    var scrollOffset = -50;
+    headerWaypoint = new Waypoint({
+                element: $body,
+                handler: function (direction) {
+                    if (direction === 'up') {
+                        $body.removeClass('scrolling');
+                    } else {
+                        $body.addClass('scrolling');
+                    }
+                },
+                offset: scrollOffset // height of global nav row
+            });
+
   });
 })(jQuery);
