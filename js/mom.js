f6c439201cb0a0794bca961078c5d26ecb237e8a
leslie tu <chiayin.l.tu@gmail.com>
2016-09-12 09:51:49 -0400

    update home page template marquee area; continue to work out SCSS config

--- js/mom.js
+++ js/mom.js
@@ -3,32 +3,36 @@
 
   $(function () {
 
-    var $marqueeAlbum = $('#marquee-album'),
-        $body = $('body'),
-        headerWaypoint;
+    var $homeSlideshow = $('#marquee-album'),
+    $body = $('body'),
+    $marqueeMain = $('.marquee-main'),
+    headerWaypoint;
 
-    $marqueeAlbum.slick({
-      adaptiveHeight: true,
-      appendArrows: $('.marquee-controls'),
-      dots: false,
-      prevArrow: '<button type="button" class="slick-prev p-abs">&#x276e;</button>',
-      nextArrow: '<button type="button" class="slick-next p-abs">&#x276f;</button>',
-      slide: '.fit-frame',
-    });
+    // $homeSlideshow.slick({
+    //   adaptiveHeight: true,
+    //   appendArrows: $('.marquee-controls'),
+    //   dots: false,
+    //   prevArrow: '<button type="button" class="slick-prev p-abs">&#x276e;</button>',
+    //   nextArrow: '<button type="button" class="slick-next p-abs">&#x276f;</button>',
+    //   slide: '.slide-frame',
+    // });
 
     // Waypoints
-    var scrollOffset = -50;
     headerWaypoint = new Waypoint({
-                element: $body,
-                handler: function (direction) {
-                    if (direction === 'up') {
-                        $body.removeClass('scrolling');
-                    } else {
-                        $body.addClass('scrolling');
-                    }
-                },
-                offset: scrollOffset // height of global nav row
-            });
+      element: $marqueeMain,
+      handler: function (direction) {
+        if (direction === 'up') {
+          $body.removeClass('scroll-down');
+        } else {
+          $body.addClass('scroll-down');
+        }
+      },
+      offset: function () {
+        var marqueeHeight = this.element.outerHeight();
+        console.log('offset '+ marqueeHeight);
+        return -marqueeHeight; 
+      }
+    });
 
   });
 })(jQuery);
